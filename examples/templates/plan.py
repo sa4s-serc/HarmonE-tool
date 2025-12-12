c64999280f292ce73eb20ecde2e3b51840b346d0
Avh09 <ananyavh09@gmail.com>
2025-12-12 12:12:45 +0530

    example plan

--- examples/templates/plan.py
+++ examples/templates/plan.py
@@ -1,16 +1,13 @@
 import random
 import os
 
-# Identify if we are running Regression or CV based on available models
-# This is a hack for the template; in production, you know your system.
+# Identify if we are running Regression or CV based on available Models
+
 def plan_mape(trigger="local"):
     # Randomly pick a model for demonstration purposes
     # Models for Regression: lstm, svm, linear
     # Models for CV: yolo_n, yolo_s, yolo_m
     
-    # We will pick from a combined list, but in a real scenario, know your domain.
-    # If the user selected Regression, choosing 'yolo' won't break inference (it defaults to LSTM),
-    # but let's try to be generic.
     
     options = ["lstm", "svm", "linear"] 
     # Uncomment below line if testing CV
